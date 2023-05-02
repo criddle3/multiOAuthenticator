70a22be4b99759bb4fee96240a5c666e96cb334c
Craig Riddle <engineering@craigriddle.com>
2023-05-02 17:31:43 -0600

    case

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='jupyterhub-multioauthenticator',
+    name='multioauthenticator',
     version='0.1',
     python_requires='>=3.9',
     packages=find_packages(),
