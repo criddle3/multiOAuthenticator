17130f9903b297e84cbf3499547db97a671ea0fb
Craig Riddle <engineering@craigriddle.com>
2023-05-08 18:42:40 -0600

    refactor load

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='multiOAuthenticator',
-    version='0.2',
-    python_requires='>=3.9',
+    version='0.2.1',
+    python_requires='>=3.10',
     packages=find_packages(),
     install_requires=[
         'oauthenticator',
